529c7db96a428d48dac529c8ffc1dfa428eb30eb
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-16 20:02:39 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,11 +3,11 @@ class Pokemon
 
   @@all = []
 
-  def initialize (id, name, type, db)
-    @id = id
-    @name = name
-    @type = type
-    @db = db
+  def initialize (keywords)
+    # @id = id
+    # @name = name
+    # @type = type
+    # @db = db
   end
 
   def self.save(name, type, db)
