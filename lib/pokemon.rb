f52094f1728163744dc72eeda5574e545e10d998
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-16 19:56:20 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,10 +21,11 @@ class Pokemon
     sql = <<-SQL
      SELECT * FROM pokemon WHERE id = (?);
    SQL
-    new_pokemon = self.new(pokemon)
+    new_pokemon = self.new(row[0], row[1], row[2])
     new_pokemon.id = row[0][0]
     new_pokemon.name = row[0][1]
     new_pokeman.type = [0][2]
     return new_pokemon
+  )
   end
 end
