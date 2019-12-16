8499b8b3200cee6fe26a32bbc2c9ff290745e7e0
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-16 19:52:17 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,29 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  @@all = []
+
+  def initialize (id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type) VALUES (?, ?);
+     SQL
+      db.execute(sql, [name, type])
+  end
+
+  def self.find (id, name, type, db)  #finds a pokemon from the database by their id number and returns a new Pokemon object
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", [num])
+    pokemon = self.new(pokemon)
+    pokemon.id = row[0][0]
+    pokemon.name = row[0][1]
+    pokeman.type = [0][2]
+    pokemon
+  end
 end
+
